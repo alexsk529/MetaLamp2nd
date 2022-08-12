baaa2c43efd8d805aadae5839aab35b4cd5fc451
Alexsk529 <alexsk529@mail.ru>
2022-08-12 16:54:51 +0500

    Finished checkbox buttons, radio buttons

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -5,6 +5,17 @@ import '../../blocks/masked-text-field/masked-text-field';
 import '../../blocks/date-dropdown/date-dropdown';
 import '../../blocks/filter-date-dropdown/filter-date-dropdown';
 import '../../blocks/subscription-text-field/subscription-text-field';
+import $ from "jquery";
+
+let arr = [];
+arr.push($('.box-and-label__label:not(:first)'));
+for (let el of arr) {
+    el.find('input').trigger('click');
+}
+
+
+
+
 
 
 
